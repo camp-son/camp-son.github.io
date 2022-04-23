a67c20199cd5f4e6859b2feab501452e6408b9f7
camp-son <sjy2814@gmail.com>
2022-04-23 17:36:21 +0900

    feat: add remove todo

--- momentum/js/toDos.js
+++ momentum/js/toDos.js
@@ -2,16 +2,32 @@ const toDoForm = document.querySelector('#toDoForm');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('#toDoList');
 
+const TODO_LIST_KEY = 'toDos';
+let toDos = [];
+
+const removeToDo = (e) => {
+    const buttonElement = e.target;
+    const liElement = buttonElement.parentNode;
+    buttonElement.removeEventListener('click', removeToDo);
+    toDos = toDos.filter((todo) => todo.id.toString() !== liElement.id);
+    liElement.remove();
+    saveToDos();
+};
+
+const saveToDos = () => {
+    localStorage.setItem(TODO_LIST_KEY, JSON.stringify(toDos));
+};
+
 const paintToDo = (todo) => {
-    const id = `${todo.id}_check`;
+    const labelId = `${todo.id}_check`;
     const toDoItemElement = document.createElement('li');
-    toDoItemElement.id = id;
+    toDoItemElement.id = todo.id;
 
     const toDoLabelElement = document.createElement('label');
-    toDoLabelElement.for = id;
+    toDoLabelElement.for = labelId;
 
     const doneCheckboxElement = document.createElement('input');
-    doneCheckboxElement.id = id;
+    doneCheckboxElement.id = labelId;
     doneCheckboxElement.type = 'checkbox';
 
     const toDoTextElement = document.createElement('span');
@@ -19,7 +35,8 @@ const paintToDo = (todo) => {
 
     const deleteButtonElement = document.createElement('button');
     deleteButtonElement.type = 'button';
-    deleteButtonElement.innerText = '❌'
+    deleteButtonElement.innerText = 'X'
+    deleteButtonElement.addEventListener('click', removeToDo);
 
     toDoLabelElement.appendChild(doneCheckboxElement);
     toDoLabelElement.appendChild(toDoTextElement);
@@ -40,5 +57,14 @@ toDoForm.addEventListener('submit', (e) => {
 
     toDoInput.value = '';
 
+    toDos.push(todo);
     paintToDo(todo);
+    saveToDos();
 });
+
+const savedToDos = JSON.parse(localStorage.getItem(TODO_LIST_KEY));
+
+if (saveToDos) {
+    toDos = savedToDos;
+    savedToDos.forEach(paintToDo);
+}
